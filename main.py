b8b97869e5d2eaa2ebae4b71b74be7fd00966b1b
Garik <garfild_2003@mail.ru>
2023-10-13 20:48:39 +0300

    Has been optimized

--- main.py
+++ main.py
@@ -6,6 +6,4 @@ from posts.blueprint import posts
 app.register_blueprint(posts, url_prefix='/blog')
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run()
