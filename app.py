d8140a0307dec6e50b5575cd3bcd1ae3b5d87839
Garik <garfild_2003@mail.ru>
2023-10-13 13:22:09 +0300

    Added migrations for DB

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from config import Configuration
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 
 app.config.from_object(Configuration)
 
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 
