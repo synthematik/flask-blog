589d660b3c92603af28ea6acf3bc1ab74602990a
Garik <garfild_2003@mail.ru>
2023-10-16 19:32:22 +0300

    Added admin page

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from config import Configuration
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 
 app = Flask(__name__)
@@ -12,4 +14,8 @@ db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 
+from models import *
+admin = Admin(app)
+admin.add_view(ModelView(Blog, db.session))
+admin.add_view(ModelView(Tag, db.session))
 
